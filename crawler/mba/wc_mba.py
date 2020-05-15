0c062764d36cb77e3dcd85c3d6549af4b5529ccb
dev_Flo <flo.t.1995@gmail.com>
2020-05-15 14:41:14 +0000

    Further development for mba script

--- crawler/mba/wc_mba.py
+++ crawler/mba/wc_mba.py
@@ -8,7 +8,7 @@ import urllib.parse as urlparse
 from urllib.parse import urlencode
 from urllib.parse import urljoin
 from utils import get_df_hobbies
-
+import mba_url_creator as url_creator
 
 def main(argv):
     parser = argparse.ArgumentParser(description='')
@@ -16,10 +16,18 @@ def main(argv):
     # get all arguments
     args = parser.parse_args()
 
-    df = get_df_hobbies("de")
-    print(df.head())
+    #df = get_df_hobbies("de")
+    df = pd.read_csv("~/mba-pipeline/crawler/mba/data/hobbies_de.csv")
+    hobbies_list = df["hobby"].tolist()
+    test_hobby = hobbies_list[4]
+
+    url_mba = url_creator.main(["Fischen", "de", "shirt", "newest"])
+
+    response = requests.get(url_mba)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
     test = 0
+
 if __name__ == '__main__':
     main(sys.argv)
 
