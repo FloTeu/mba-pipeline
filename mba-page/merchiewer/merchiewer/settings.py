c3729e28610ffd768c08d62c8e14af40d97d72d4
dev_Flo <florianteutsch@yahoo.de>
2020-06-13 13:23:10 +0000

    Integrate index filtering for lists

--- mba-page/merchiewer/merchiewer/settings.py
+++ mba-page/merchiewer/merchiewer/settings.py
@@ -63,6 +63,11 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+        'libraries':{
+            'index': 'merchiewer.templatetags.index',
+
+            }
         },
     },
 ]
