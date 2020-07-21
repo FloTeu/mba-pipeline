e39b54923c14ff901cd9a3dd40e2e7878b7831c6
dev_Flo <florianteutsch@yahoo.de>
2020-07-21 11:09:38 +0000

    Include detail_daily information in mba page

---
+++ mba-page/merchwatch/merchwatch/data_handler.py
@@ -0,0 +1,147 @@
+import pandas as pd
+from google.cloud import bigquery
+import itertools
+from sklearn import preprocessing
+import os 
+from datetime import date
+import datetime 
+import time 
+
+
+class DataHandler():
+    def __init__(self):
+        self.filePath = None
+
+    def get_sql_shirts(self, marketplace, limit=None, filter=None):
+        if limit == None:
+            SQL_LIMIT = ""
+        elif type(limit) == int and limit > 0:
+            SQL_LIMIT = "LIMIT " + str(limit)
+        else:
+            assert False, "limit is not correctly set"
+
+        
+        if filter == None:
+            SQL_WHERE= "where bsr != 0 and bsr != 404"
+        elif filter == "only 404":
+            SQL_WHERE = "where bsr = 404"
+        elif filter == "only 0":
+            SQL_WHERE = "where bsr = 0"
+        else:
+            assert False, "filter is not correctly set"
+
+        SQL_STATEMENT = """
+        SELECT t_fin.* FROM (
+    SELECT t0.*, t2.title, DATE_DIFF(current_date(), Date(t2.upload_date), DAY) as time_since_upload,Date(t2.upload_date) as upload_date, t2.product_features, t1.url, t1.url_mba_hq, t1.url_mba_lowq FROM (
+        SELECT asin, AVG(price) as price_mean,MAX(price) as price_max,MIN(price) as price_min,
+                AVG(bsr) as bsr_mean, MAX(bsr) as bsr_max,MIN(bsr) as bsr_min, COUNT(*) as bsr_count,
+                AVG(customer_review_score_mean) as score_mean, MAX(customer_review_score_mean) as score_max, MIN(customer_review_score_mean) as score_min 
+                FROM `mba-pipeline.mba_{0}.products_details_daily`
+        where bsr != 0 and bsr != 404
+        group by asin
+        ) t0
+        left join `mba-pipeline.mba_de.products_images` t1 on t0.asin = t1.asin
+        left join `mba-pipeline.mba_de.products_details` t2 on t0.asin = t2.asin
+
+        union all 
+        
+        SELECT t0.*, t2.title, DATE_DIFF(current_date(), Date(t2.upload_date), DAY) as time_since_upload,Date(t2.upload_date) as upload_date, t2.product_features, t1.url, t1.url_mba_hq, t1.url_mba_lowq FROM (
+        SELECT asin, AVG(price) as price_mean,MAX(price) as price_max,MIN(price) as price_min,
+                AVG(bsr) as bsr_mean, MAX(bsr) as bsr_max,MIN(bsr) as bsr_min, COUNT(*) as bsr_count,
+                AVG(customer_review_score_mean) as score_mean, MAX(customer_review_score_mean) as score_max, MIN(customer_review_score_mean) as score_min 
+                FROM `mba-pipeline.mba_{0}.products_details_daily`
+        where bsr = 0 and bsr != 404
+        and asin NOT IN (SELECT asin FROM `mba-pipeline.mba_de.products_details_daily` WHERE bsr != 0 and bsr != 404 group by asin)
+        group by asin
+        ) t0
+        left join `mba-pipeline.mba_de.products_images` t1 on t0.asin = t1.asin
+        left join `mba-pipeline.mba_de.products_details` t2 on t0.asin = t2.asin
+        
+        ) t_fin
+        order by t_fin.bsr_mean
+        {1}
+        """.format(marketplace, SQL_LIMIT)
+        return SQL_STATEMENT
+
+    def get_sql_shirts_detail_daily(self, marketplace, limit=None, filter=None):
+        if limit == None:
+            SQL_LIMIT = ""
+        elif type(limit) == int and limit > 0:
+            SQL_LIMIT = "LIMIT " + str(limit)
+        else:
+            assert False, "limit is not correctly set"
+
+        SQL_STATEMENT = """
+        SELECT asin, price, bsr, price_str, bsr_str, timestamp FROM `mba-pipeline.mba_{0}.products_details_daily`
+        order by timestamp desc
+        {1}
+        """.format(marketplace, SQL_LIMIT)
+        return SQL_STATEMENT
+
+    def make_trend_column(self, df_shirts):
+        df_shirts = df_shirts.reset_index()
+        x = df_shirts[["time_since_upload"]].values 
+        min_max_scaler = preprocessing.MinMaxScaler()
+        x_scaled = min_max_scaler.fit_transform(x)
+        df = pd.DataFrame(x_scaled)
+        df_shirts["time_since_upload_norm"] = df.iloc[:,0]
+        df_shirts["trend"] = df_shirts["bsr_last"] * df_shirts["time_since_upload_norm"] * 2
+        return df_shirts
+
+    def check_if_shirts_today_exist(self, file_path):
+        try:
+            date_creation = time.ctime(os.path.getctime(file_path))
+            return date.today() == datetime.datetime.strptime(date_creation, "%a %b %d %H:%M:%S %Y").date()
+        except:
+            return False
+
+    def get_shirts(self, marketplace, limit=None, in_test_mode=False, filter=None):
+        print(os.getcwd())
+        file_path = "merchwatch/data/shirts.csv"
+        
+        if self.check_if_shirts_today_exist(file_path):
+            print("Data already loaded today")
+            df_shirts=pd.read_csv("merchwatch/data/shirts.csv", sep="\t")
+            df_shirts_detail_daily=pd.read_csv("merchwatch/data/shirts_detail_daily.csv", sep="\t")
+        else:
+            print("Load shirt data from bigquery")
+            project_id = 'mba-pipeline'
+            bq_client = bigquery.Client(project=project_id)
+            df_shirts = bq_client.query(self.get_sql_shirts(marketplace, limit, filter)).to_dataframe().drop_duplicates()
+            df_shirts_detail_daily = bq_client.query(self.get_sql_shirts_detail_daily(marketplace, limit)).to_dataframe().drop_duplicates()
+            df_shirts.to_csv("merchwatch/data/shirts_detail_daily.csv", index=None, sep="\t")
+            def get_first_and_last_data(asin):
+                # return last_bsr, last_price, first_bsr, first_price
+                occurences = (df_shirts_detail_daily.asin.values == asin)
+                if len(df_shirts_detail_daily[occurences]) == 0:
+                    return 0,0,0,0
+                else:
+                    last_occ = df_shirts_detail_daily[occurences].iloc[0]
+                first_occ = df_shirts_detail_daily[occurences].iloc[-1]
+                return last_occ["bsr"], last_occ["price"], first_occ["bsr"], first_occ["price"]
+
+            #df_shirts2 = df_shirts.copy()
+            df_additional_data = df_shirts.apply(lambda x: pd.Series(get_first_and_last_data(x["asin"])), axis=1)
+            df_additional_data.columns=["bsr_last", "price_last", "bsr_first", "price_first"]
+            df_shirts = df_shirts.merge(df_additional_data, 
+                left_index=True, right_index=True)
+            df_shirts = self.make_trend_column(df_shirts)
+            df_shirts.to_csv("merchwatch/data/shirts.csv", index=None, sep="\t")
+            print("Loading completed.")
+            #df_shirts[df_shirts["bsr_mean"] != 0][["trend", "time_since_upload","time_since_upload_norm", "bsr_mean"]].head(10)
+        
+        return df_shirts, df_shirts_detail_daily
+'''
+dataHandleModel = DataHandler()
+#dataHandleModel.get_sql_shirts("de", None)
+project_id = 'mba-pipeline'
+bq_client = bigquery.Client(project=project_id)
+marketplace = "de"
+limit = None
+filter=None
+df_shirts = bq_client.query(dataHandleModel.get_sql_shirts(marketplace, limit, filter)).to_dataframe().drop_duplicates()
+df_shirts_detail_daily = bq_client.query(dataHandleModel.get_sql_shirts_detail_daily(marketplace, limit)).to_dataframe().drop_duplicates()
+
+#df_shirts2["bsr_last"], df_shirts2["price_last"], df_shirts2["bsr_first"], df_shirts2["price_first"] = df_shirts2.apply(lambda x: get_first_and_last_data(x["asin"]), axis=1)
+'''
+test = 0
--- mba-page/merchwatch/merchwatch/views.py
+++ mba-page/merchwatch/merchwatch/views.py
@@ -10,6 +10,8 @@ import os
 from datetime import date
 import datetime 
 import time 
+from .data_handler import DataHandler 
+
 
 register = template.Library()
 
@@ -19,105 +21,13 @@ register = template.Library()
 
 def about(request):
     return HttpResponse("about")
-
-
-def get_sql(marketplace, limit, filter=None):
-    if limit == None:
-        SQL_LIMIT = ""
-    elif type(limit) == int and limit > 0:
-        SQL_LIMIT = "LIMIT " + str(limit)
-    else:
-        assert False, "limit is not correctly set"
-
-    
-    if filter == None:
-        SQL_WHERE= "where bsr != 0 and bsr != 404"
-    elif filter == "only 404":
-        SQL_WHERE = "where bsr = 404"
-    elif filter == "only 0":
-        SQL_WHERE = "where bsr = 0"
-    else:
-        assert False, "filter is not correctly set"
-
-    SQL_STATEMENT = """
-    SELECT t_fin.* FROM (
-SELECT t0.*, t2.title, DATE_DIFF(current_date(), Date(t2.upload_date), DAY) as time_since_upload,Date(t2.upload_date) as upload_date, t2.product_features, t1.url FROM (
-    SELECT asin, AVG(price) as price_mean,MAX(price) as price_max,MIN(price) as price_min,
-            AVG(bsr) as bsr_mean, MAX(bsr) as bsr_max,MIN(bsr) as bsr_min, COUNT(*) as bsr_count,
-            AVG(customer_review_score_mean) as score_mean, MAX(customer_review_score_mean) as score_max, MIN(customer_review_score_mean) as score_min 
-            FROM `mba-pipeline.mba_{0}.products_details_daily`
-    where bsr != 0 and bsr != 404
-    group by asin
-    ) t0
-    left join `mba-pipeline.mba_de.products_images` t1 on t0.asin = t1.asin
-    left join `mba-pipeline.mba_de.products_details` t2 on t0.asin = t2.asin
-  
-    
-    union all 
-    
-    SELECT t0.*, t2.title, DATE_DIFF(current_date(), Date(t2.upload_date), DAY) as time_since_upload,Date(t2.upload_date) as upload_date, t2.product_features, t1.url FROM (
-    SELECT asin, AVG(price) as price_mean,MAX(price) as price_max,MIN(price) as price_min,
-            AVG(bsr) as bsr_mean, MAX(bsr) as bsr_max,MIN(bsr) as bsr_min, COUNT(*) as bsr_count,
-            AVG(customer_review_score_mean) as score_mean, MAX(customer_review_score_mean) as score_max, MIN(customer_review_score_mean) as score_min 
-            FROM `mba-pipeline.mba_{0}.products_details_daily`
-    where bsr = 0 and bsr != 404
-    and asin NOT IN (SELECT asin FROM `mba-pipeline.mba_de.products_details_daily` WHERE bsr != 0 and bsr != 404 group by asin)
-    group by asin
-    ) t0
-    left join `mba-pipeline.mba_de.products_images` t1 on t0.asin = t1.asin
-    left join `mba-pipeline.mba_de.products_details` t2 on t0.asin = t2.asin
-    
-    ) t_fin
-    order by t_fin.bsr_mean
-    {1}
-    """.format(marketplace, SQL_LIMIT)
-    return SQL_STATEMENT
-
-def make_trend_column(df_shirts):
-    df_shirts = df_shirts.reset_index()
-    x = df_shirts[["time_since_upload"]].values 
-    min_max_scaler = preprocessing.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(x)
-    df = pd.DataFrame(x_scaled)
-    df_shirts["time_since_upload_norm"] = df.iloc[:,0]
-    df_shirts["trend"] = df_shirts["bsr_mean"] * df_shirts["time_since_upload_norm"] * 2
-    return df_shirts
-
-def check_if_shirts_today_exist(file_path):
-    date_creation = time.ctime(os.path.getctime(file_path))
-    return date.today() == datetime.datetime.strptime(date_creation, "%a %b %d %H:%M:%S %Y").date()
-
-def get_shirts(marketplace, limit=None, in_test_mode=False, filter=None):
-    print(os.getcwd())
-    file_path = "merchwatch/data/shirts.csv"
-    if check_if_shirts_today_exist(file_path):
-        print("Data already loaded today")
-        df_shirts=pd.read_csv("merchwatch/data/shirts.csv", sep="\t")
-    else:
-        print("Load shirt data from bigquery")
-        project_id = 'mba-pipeline'
-        bq_client = bigquery.Client(project=project_id)
-        df_shirts = bq_client.query(get_sql(marketplace, limit, filter)).to_dataframe().drop_duplicates()
-        df_shirts = make_trend_column(df_shirts)
-        df_shirts.to_csv("merchwatch/data/shirts.csv", index=None, sep="\t")
-        print("Loading completed.")
-        #df_shirts[df_shirts["bsr_mean"] != 0][["trend", "time_since_upload","time_since_upload_norm", "bsr_mean"]].head(10)
-    
-    return df_shirts
-
-    '''
-    if in_test_mode:
-        df_shirts=pd.read_csv("merchwatch/data/shirts2.csv", sep="\t")
-    else:
-        project_id = 'mba-pipeline'
-        bq_client = bigquery.Client(project=project_id)
-        df_shirts = bq_client.query(get_sql(marketplace, limit, filter)).to_dataframe().drop_duplicates()
-    '''
     
 def main(request):
     iterator=itertools.count()
     marketplace = "de"
     
+    DataHandlerModel = DataHandler()
+
     sort_by = request.GET.get('sort_by')
     desc = request.GET.get('direction')
     info = request.GET.get('info')
@@ -132,7 +42,7 @@ def main(request):
         filter = "only 404"
     #q_desc = request.GET["direction"]
 
-    df_shirts = get_shirts(marketplace, limit=None, in_test_mode=True, filter=filter)
+    df_shirts, df_shirts_detail_daily = DataHandlerModel.get_shirts(marketplace, limit=None, in_test_mode=True, filter=filter)
     df_shirts = df_shirts.round(2)
 
     if key != None:
@@ -142,12 +52,12 @@ def main(request):
     if sort_by != None:
         if desc == "desc":
             if "bsr" in sort_by or "trend" in sort_by: 
-                df_shirts = df_shirts[df_shirts["bsr_max"]!=0].sort_values(sort_by, ascending=False)
+                df_shirts = df_shirts[(df_shirts["bsr_max"]!=0) & (df_shirts["bsr_last"]!=404)].sort_values(sort_by, ascending=False)
             else:
                 df_shirts = df_shirts.sort_values(sort_by, ascending=False)
         else:
             if "bsr" in sort_by or "trend" in sort_by: 
-                df_shirts = df_shirts[df_shirts["bsr_max"]!=0].sort_values(sort_by, ascending=True)
+                df_shirts = df_shirts[(df_shirts["bsr_max"]!=0) & (df_shirts["bsr_last"]!=404)].sort_values(sort_by, ascending=True)
             else:
                 df_shirts = df_shirts.sort_values(sort_by, ascending=True)
 
@@ -167,7 +77,7 @@ def main(request):
         
     shirt_info = df_shirts.to_dict(orient='list')
     #context = {"asin": ["awdwa","awdwawdd", "2312313"],}
-    return render(request, 'main_boss.html', {"shirt_info":shirt_info, "iterator":iterator, "columns" : columns, "rows": rows,"show_detail_info":info, "sort_by":sort_by})
+    return render(request, 'main.html', {"shirt_info":shirt_info, "iterator":iterator, "columns" : columns, "rows": rows,"show_detail_info":info, "sort_by":sort_by})
     #return HttpResponse(template.render(context, request))
 
 #df_shirts = get_shirts("de", limit=None, in_test_mode=False)
