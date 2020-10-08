80957f5e1a55c37596c63fad01edf60e5b02f758
dev_Flo <florianteutsch@yahoo.de>
2020-10-08 16:20:36 +0000

    update crawler + implement script for creating url csv

---
+++ crawler/mba/mba_crawler/mba_crawler/create_url_csv.py
@@ -0,0 +1,98 @@
+from bs4 import BeautifulSoup
+import requests 
+import pandas as pd
+import numpy as np
+import argparse
+import sys
+import random 
+from google.cloud import storage
+from google.cloud import bigquery
+import os
+import time 
+import dateparser
+import datetime
+
+
+def does_table_exist(project_id, dataset_id, table_id):
+    client = bigquery.Client()
+    try:
+        df = client.query("SELECT * FROM %s.%s.%s" %(project_id, dataset_id, table_id)).to_dataframe().drop_duplicates()
+        return True
+    except Exception as e:
+        return False
+
+def get_asin_product_detail_daily_crawled(marketplace):
+    project_id = 'mba-pipeline'
+    reservationdate = datetime.datetime.now()
+    dataset_id = "preemptible_logs"
+    table_id = "mba_detail_daily_" + marketplace + "_preemptible_%s_%s_%s"%(reservationdate.year, reservationdate.month, reservationdate.day)
+    reservation_table_id = dataset_id + "." + table_id
+    bq_client = bigquery.Client(project=project_id)
+    # TODO get those which are not already crawled today
+    # TODO remove asins that return a 404 (not found) error
+    SQL_STATEMENT = '''
+    SELECT t0.asin, t0.url_product, t2.bsr_count FROM mba_{0}.products t0 LEFT JOIN (SELECT * FROM mba_{0}.products_details_daily WHERE DATE(timestamp) = '{1}-{2}-{3}' or price_str = '404') t1 on t0.asin = t1.asin 
+        LEFT JOIN 
+        (
+        SELECT asin, AVG(price) as price_mean,MAX(price) as price_max,MIN(price) as price_min,
+                    AVG(bsr) as bsr_mean, MAX(bsr) as bsr_max,MIN(bsr) as bsr_min, COUNT(*) as bsr_count,
+                    AVG(customer_review_score_mean) as score_mean, MAX(customer_review_score_mean) as score_max, MIN(customer_review_score_mean) as score_min 
+                    FROM `mba-pipeline.mba_{0}.products_details_daily`
+            where bsr != 404
+            group by asin
+        ) t2 on t0.asin = t2.asin 
+        where t1.asin IS NULL 
+        order by t2.bsr_count
+    '''.format(marketplace, reservationdate.year, reservationdate.month, reservationdate.day)
+    #df_product_details = bq_client.query("SELECT t0.asin, t0.url_product FROM mba_" + marketplace + ".products t0 LEFT JOIN (SELECT * FROM mba_" + marketplace + ".products_details_daily WHERE DATE(timestamp) = '%s-%s-%s' or price_str = '404') t1 on t0.asin = t1.asin where t1.asin IS NULL order by t0.timestamp" %(reservationdate.year, reservationdate.month, reservationdate.day)).to_dataframe().drop_duplicates()
+    df_product_details = bq_client.query(SQL_STATEMENT).to_dataframe().drop_duplicates()
+
+    if does_table_exist(project_id, dataset_id, table_id):
+        # get reservation logs
+        df_reservation = bq_client.query("SELECT * FROM " + reservation_table_id + " t0 order by t0.timestamp DESC").to_dataframe().drop_duplicates()
+        df_reservation_status = df_reservation.drop_duplicates("asin")
+        # get list of asins that are currently blocked by preemptible instances
+        asins_blocked = df_reservation_status[df_reservation_status["status"] == "blocked"]["asin"].tolist()
+        # filter asins for those which are not blocked
+        matching_asins = df_product_details["asin"].isin(asins_blocked)
+        print("%s asins are currently blocked and will not be crawled" % str(len([i for i in matching_asins if i == True])))
+        df_product_details = df_product_details[~matching_asins]
+    
+    return df_product_details
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('marketplace', help='Shortcut of mba marketplace. I.e "com" or "de", "uk"', type=str)
+    parser.add_argument('--number_products', default=10, type=int, help='Number of products/shirts that shoul be crawled. If 0, every image that is not already crawled will be crawled.')
+
+    # if python file path is in argv remove it 
+    if ".py" in argv[0]:
+        argv = argv[1:len(argv)]
+
+    # get all arguments
+    args = parser.parse_args(argv)
+    marketplace = args.marketplace
+    number_products = args.number_products
+
+    # get all arguments
+    args = parser.parse_args()
+
+    # get asins which are not already crawled
+    df_product_details_tocrawl_total = get_asin_product_detail_daily_crawled(marketplace)
+    df_product_details_tocrawl = df_product_details_tocrawl_total[0:int(number_products/2)].reset_index(drop=True)
+    # remove asins that are in priority order
+    df_product_details_tocrawl_total = df_product_details_tocrawl_total[~df_product_details_tocrawl_total.asin.isin(df_product_details_tocrawl["asin"].tolist())]
+    df_product_details_tocrawl = df_product_details_tocrawl.append(df_product_details_tocrawl_total.sample(frac=1).reset_index(drop=True))
+
+    #df_product_details = pd.DataFrame(data={"asin": ["B07RVNJHZL"], "url_product": ["adwwadwad"]})
+    df_product_details_tocrawl["url"] =  df_product_details_tocrawl.apply(lambda x: "https://www.amazon."+marketplace+"/dp/"+x["asin"], axis=1)
+    
+    # if number_images is equal to 0, evry image should be crawled
+    if number_products == 0:
+        number_products = len(df_product_details_tocrawl)
+
+    df_product_details_tocrawl[["url", "asin"]].iloc[0:number_products].to_csv("urls.csv",index=False)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
--- crawler/mba/mba_crawler/mba_crawler/spiders/amazon_spider.py
+++ crawler/mba/mba_crawler/mba_crawler/spiders/amazon_spider.py
@@ -23,10 +23,11 @@ class AmazonSpider(scrapy.Spider):
     df_products_details = pd.DataFrame(data={"asin":[],"price":[],"price_str":[],"bsr":[],"bsr_str":[], "array_bsr": [], "array_bsr_categorie": [],"customer_review_score_mean":[],"customer_review_score": [],"customer_review_count": [], "timestamp":[]})
     target="869595848"
     api_key="1266137258:AAH1Yod2nYYud0Vy6xOzzZ9LdR7Dvk9Z2O0"
+    ip_addresses = []
 
     def start_requests(self):
-        urls = pd.read_csv("mba_crawler/urls_small.csv")["url"].tolist()
-        asins = pd.read_csv("mba_crawler/urls_small.csv")["asin"].tolist()
+        urls = pd.read_csv("mba_crawler/urls.csv")["url"].tolist()
+        asins = pd.read_csv("mba_crawler/urls.csv")["asin"].tolist()
         send_msg(self.target, "Start scraper {} with {} products".format(self.name, len(urls)), self.api_key)
         for i, url in enumerate(urls):
             #proxies = proxy_handler.get_random_proxy_url_dict()
@@ -45,17 +46,29 @@ class AmazonSpider(scrapy.Spider):
         if failure.check(HttpError):
             # you can get the response
             response = failure.value.response
+            try:
+                # if 404 update big query
+                if response.status == 404:
+                    crawlingdate = datetime.datetime.now()
+                    df = pd.DataFrame(data={"asin":[response.meta["asin"]],"price":[404.0],"price_str":["404"],"bsr":[404],"bsr_str":["404"], "array_bsr": [["404"]], "array_bsr_categorie": [["404"]],"customer_review_score_mean":[404.0],"customer_review_score": ["404"],"customer_review_count": [404], "timestamp":crawlingdate})
+                    self.df_products_details = self.df_products_details.append(df)
+                else:
+                    send_msg(self.target, "HttpError on asin: {} | status_code: {} | ip address: {}".format(response.meta["asin"], response.status, response.ip_address.compressed), self.api_key)
+            except:
+                pass
             self.logger.error('HttpError on %s', response.url)
 
         #elif isinstance(failure.value, DNSLookupError):
         elif failure.check(DNSLookupError):
             # this is the original request
             request = failure.request
+            send_msg(self.target, "DNSLookupError on url: {}".format(request.url), self.api_key)
             self.logger.error('DNSLookupError on %s', request.url)
 
         #elif isinstance(failure.value, TimeoutError):
         elif failure.check(TimeoutError):
             request = failure.request
+            send_msg(self.target, "TimeoutError on url: {}".format(request.url), self.api_key)
             self.logger.error('TimeoutError on %s', request.url)
 
     def get_price(self, response):
@@ -121,6 +134,7 @@ class AmazonSpider(scrapy.Spider):
         return customer_review_score_mean, customer_review_score, customer_review_count
 
     def parse(self, response):
+        self.ip_addresses.append(response.ip_address.compressed)
         asin = response.meta["asin"]
         try:
             price_str, price = self.get_price(response)
@@ -140,6 +154,15 @@ class AmazonSpider(scrapy.Spider):
         self.df_products_details = self.df_products_details.append(df)
 
     def closed(self, reason):
+        try:
+            ip_dict = {i:self.ip_addresses.count(i) for i in self.ip_addresses}
+            ip_addr_str=""
+            for ip, count in ip_dict.items():
+                ip_addr_str = "{}{}: {}\n".format(ip_addr_str, ip, count)
+            #ip_addresses_str = "\n".join(list(set(self.ip_addresses)))
+            send_msg(self.target, "Used ip addresses: \n{}".format(ip_addr_str), self.api_key)
+        except:
+            pass
         send_msg(self.target, "Finished scraper {} with {} products".format(self.name, len(self.df_products_details)), self.api_key)
         self.df_products_details['timestamp'] = self.df_products_details['timestamp'].astype('datetime64')
         self.df_products_details['bsr'] = self.df_products_details['bsr'].astype('int')
