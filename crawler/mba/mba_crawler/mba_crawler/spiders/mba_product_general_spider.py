b1e162f9698e9fd6184a22096cdd98eb182d1d24
dev_Flo <florianteutsch@yahoo.de>
2020-11-01 11:35:55 +0000

    Update general crawler ban regulation

--- crawler/mba/mba_crawler/mba_crawler/spiders/mba_product_general_spider.py
+++ crawler/mba/mba_crawler/mba_crawler/spiders/mba_product_general_spider.py
@@ -172,6 +172,9 @@ class MBASpider(scrapy.Spider):
             is_ban = True 
         proxy = self.get_proxy(request)
         is_ban = self.was_already_banned(proxy)
+        if response.status == 503:
+            self.update_ban_count(proxy)
+            is_ban = True
         if is_ban:
             print("Ban proxy: " + proxy)
         should_be_banned = b'banned' in response.body or is_ban
