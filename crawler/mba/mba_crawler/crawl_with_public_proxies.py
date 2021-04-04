64117829ca4e3b0e1b354dc381ef14ca29fe9e80
dev_Flo <florianteutsch@yahoo.de>
2021-04-04 12:37:58 +0000

    update crawler public

--- crawler/mba/mba_crawler/crawl_with_public_proxies.py
+++ crawler/mba/mba_crawler/crawl_with_public_proxies.py
@@ -10,6 +10,8 @@ import os
 import random
 
 DAY, NIGHT = 1, 2
+MARKETPLACES = ["com", "de"]
+
 def check_time(time_to_check, on_time, off_time):
     if on_time > off_time:
         if time_to_check > on_time or time_to_check < off_time:
@@ -37,7 +39,7 @@ def replace(file_path, pattern, subst):
 
 def main(argv):
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('marketplace', help='Shortcut of mba marketplace. I.e "com" or "de", "uk"', type=str)
+    parser.add_argument('marketplace', help='Shortcut of mba marketplace. I.e "com" or "de", "uk", "all"', type=str)
     parser.add_argument('--number_products', default=10, type=int, help='Number of products/shirts that shoul be crawled. If -1, every image that is not already crawled will be crawled.')
     parser.add_argument('--proportion_priority_low_bsr_count', default=0, type=float, help='50% is the default proportion what means 50% should be design which were crawled least often')
     parser.add_argument('--repeat', default=1, type=int, help='If crawling should be repeated')
@@ -46,9 +48,12 @@ def main(argv):
     if ".py" in argv[0]:
         argv = argv[1:len(argv)]
 
+    marketplace_rotation = False
     # get all arguments
     args = parser.parse_args(argv)
     marketplace = args.marketplace
+    if marketplace == "all":
+        marketplace_rotation = True
     repeat = args.repeat
     number_products = args.number_products
     proportion_priority_low_bsr_count = args.proportion_priority_low_bsr_count
@@ -59,14 +64,19 @@ def main(argv):
     off_time = datetime.time(10,00)
     
     replace("mba_crawler/settings.py", "CONCURRENT_REQUESTS = 5", "CONCURRENT_REQUESTS = 10")
+    count = 0
+    # is endless while loop if repeat is True/1
     while_condition = True
-    while while_condition:    
+    while while_condition: 
+        if marketplace_rotation:    
+            marketplace = MARKETPLACES[count%len(MARKETPLACES)]  
         current_time = datetime.datetime.now().time()
         when, matching = check_time(current_time, on_time, off_time)
+        #matching = True
         # execute function only between on_time and off_time
         if matching or not repeat:
             # sleep random to prevent com crawler and de crawler change their settings
-            time.sleep(random.randint(0,60))
+            #time.sleep(random.randint(0,60))
             # create crawling data csv
             command = """sudo python3 create_url_csv.py {0} True --number_products={1} --proportion_priority_low_bsr_count={2}
             """.format(marketplace, number_products, proportion_priority_low_bsr_count)
@@ -87,6 +97,7 @@ def main(argv):
             
             if not repeat:
                 while_condition = False
+            count = count + 1
         else:
             print("Sleep for half an hour")
             time.sleep(30*60)
