9c8d5e0f552c563611bf06edcac689c049b3ecf5
dev_Flo <f.teutsch@marketvision.ai>
2022-01-09 20:20:21 +0100

    memory optimization

--- mwfunctions/io/io_fns.py
+++ mwfunctions/io/io_fns.py
@@ -1,5 +1,7 @@
 import shutil, errno
 import os
+import json
+
 
 def copy_dir2dir(src, dst):
     try:
@@ -24,3 +26,11 @@ def str2bool(v):
     else:
         # maybe argparse.ArgumentTypeError('Boolean value expected.') must be raised in case of argparse
         raise ValueError("Provided argument is not a bool")
+
+def save_dict2json_file(data_dict, dest_file_path="dict.json"):
+    with open(dest_file_path, 'w') as fp:
+        json.dump(data_dict, fp)
+
+def read_json_file(src_file_path):
+    with open(src_file_path, 'r') as fp:
+        return json.load(fp)
